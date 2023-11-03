57d2b2642c299532f294a6caf7416c62b735bf6c
Gospel Darlington <darlingtongospel@gmail.com>
2023-11-03 12:59:26 +0000

    modifies the login function

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import './Login.css';
-import linkedIn from './assets/linkedin.png'
-import { auth } from './firebase';
-import { useDispatch } from 'react-redux';
-import { userSlice } from './store/userSlice';
+import React, { useState } from "react";
+import "./Login.css";
+import linkedIn from "./assets/linkedin.png";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { userActions } from "./store/userSlice";
 
 const Login = () => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [profilePic, setProfilePic] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [profilePic, setProfilePic] = useState("");
   const dispatch = useDispatch();
-  const { login } = userSlice
+
+  const { login } = userActions;
 
   const loginToApp = (e) => {
     e.preventDefault();
@@ -20,35 +20,33 @@ const Login = () => {
 
   const register = () => {
     if (!name) {
-      return alert('please enter a full name!');
+      return alert("please enter a full name!");
     }
 
-    auth.createUserWithEmailAndPassword(email, password)
-    .then((userAuth) =>{
-      userAuth.user.updateProfile({
-        displayName: name,
-        photoURL: profilePic,
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: profilePic,
+              })
+            );
+          });
       })
-      .then(() =>{
-        console.log({
-          email: userAuth.user.email,
-          uid: userAuth.user.uid,
-          displayName: name,
-          photoURL: profilePic
-        })
-        dispatch(login({
-          email: userAuth.user.email,
-          uid: userAuth.user.uid,
-          displayName: name,
-          photoURL: profilePic
-        }));
-      });
-    })
-    .catch((error) => alert(error));
+      .catch((error) => alert(error));
   };
 
   return (
-    <div className='login'>
+    <div className="login">
       <img src={linkedIn} alt="" />
 
       <form>
@@ -77,14 +75,19 @@ const Login = () => {
           type="password"
         />
 
-        <button type='submit' onClick={loginToApp}>Sign In</button>
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
       </form>
 
-      <p>If you are not a member?{''}
-        <span className='login__register' onClick={register}>Register Now</span>
+      <p>
+        If you are not a member?{""}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
       </p>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
